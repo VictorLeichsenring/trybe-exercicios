46079ff21b68528b17038bf80d8a4353e6eee7f2
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-03-04 00:07:38 -0300

    finalizado

---
+++ 3-back-end/secao-07-introducao-a-typescript/dia-01-aprendendo-typescript/conteudo/hello/exercise.ts
@@ -0,0 +1,13 @@
+type StatesOfMatter = "liquid" | "solid" | "gaseous";
+type IdentifyingDocument = string | number;
+type SO = "Linux" | "MacOS" | "Windows";
+
+type ReservationNames = string[] | string;
+
+type Address = {
+  publicPlace: string;
+  number: number;
+  district: string;
+  city: string;
+  state: string;
+}
