d317b46425582fd21cd43b9a6740d9aabb975a56
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-22 01:23:34 -0300

    finalizado

---
+++ 3-back-end/secao-08-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/conteudo/problemaInicial.ts
@@ -0,0 +1,63 @@
+class Email {
+  private _from: string;
+  private _to: string;
+  private _message: string;
+  private _subject: string;
+
+  constructor(
+    from: string,
+    to: string,
+    subject: string,
+    message: string,
+  ) {
+    this._from = from;
+    this._to = to;
+    this.subject = subject;
+    this._message = message;
+  }
+
+  set subject(newSubject: string) {
+    if (newSubject.length <= 40) this.subject = newSubject;
+  }
+
+  get subject(): string {
+    return this._subject;
+  }
+
+  get from(): string { return this._from; }
+
+  get to(): string { return this._to; }
+
+  get content(): string {
+    return `
+    From ${this._from} to ${this._to}
+    ${this.subject}
+
+    ${this._message}`;
+  }
+}
+
+class MailList {
+  constructor(private mailList: Email[] = []) { }
+
+  get all(): Email[] { return [...this.mailList]; }
+
+  getEmailsBySender(mailAddress: string):Email[] {
+    return this.mailList.filter((mail) => mail.from === mailAddress);
+  }
+
+  getEmailsTo(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.to === mailAddress);
+  }
+
+  getEmailsBySybject(searchString: string): Email[] {
+    return this.mailList
+    .filter((mail) => mail.subject.indexOf(searchString) !== -1);
+  }
+
+  addEmail(newMail: Email): void { this.mailList.push(newMail); }
+
+  removeEmail(mailToRemove: Email): void {
+    this.mailList = this.mailList.filter((mail) =>  mail !== mailToRemove)
+  }
+}
